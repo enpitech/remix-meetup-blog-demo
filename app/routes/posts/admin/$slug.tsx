2b6235d6890a53a63ccca30e1507d7afb79469dd
Rotem Barmaimon <rotembm12@gmail.com>
2022-10-29 13:44:56 +0300

    a lot of progress

--- app/routes/posts/admin/$slug.tsx
+++ app/routes/posts/admin/$slug.tsx
@@ -1,19 +1,16 @@
-import {
-  Form,
-  useActionData,
-  useLoaderData,
-  useTransition,
-} from "@remix-run/react";
+import { Form, useLoaderData, useTransition } from "@remix-run/react";
+
 import {
   ActionFunction,
   json,
   LoaderFunction,
   redirect,
 } from "@remix-run/server-runtime";
+
 import invariant from "tiny-invariant";
 import { deletePost, getPost, Post, updatePost } from "~/models/post.server";
 
-export const loader: LoaderFunction = async ({ request, params }) => {
+export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.slug, "Slug must be defined");
 
   const post = await getPost(params.slug);
@@ -34,12 +31,14 @@ export const action: ActionFunction = async ({ request, params }) => {
     }
     case "PUT": {
       const formData = await request.formData();
+
       const post = Object.fromEntries(formData);
 
       await updatePost(
         params.slug,
         post as Pick<Post, "slug" | "title" | "markdown">
       );
+
       return redirect(`/posts/${post.slug}`);
     }
   }
@@ -51,13 +50,16 @@ export default function PostSlug() {
   const transition = useTransition();
   const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
   const isSubmitting = transition.state === "submitting";
+  const isDeleting = isSubmitting && transition.submission?.method === "DELETE";
 
   return (
     <div>
       <h1>{post.title}</h1>
 
       <Form method="delete">
-        <button className="bg-red-500 p-3 text-white">Delete</button>
+        <button className="bg-red-500 p-3 text-white">
+          {isDeleting ? "Deleting..." : "Delete"}
+        </button>
       </Form>
 
       <Form method="put">
@@ -99,7 +101,7 @@ export default function PostSlug() {
             type="submit"
             className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
           >
-            {isSubmitting ? "Updating...." : "Update"}
+            {isSubmitting && !isDeleting ? "Updating...." : "Update"}
           </button>
         </p>
       </Form>
